f2b035ec4ab72ba9846636ad5d2bcc72511f0e1b
shinp09 <57585657+shinp09@users.noreply.github.com>
2022-02-20 14:30:23 +0900

    feat: IDを指定してレコードの編集ができるようにした

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/tenntenn/sqlite"
@@ -21,6 +22,7 @@ func main() {
 }
 
 func run() error {
+	var input string
 	db, err := sql.Open(sqlite.DriverName, "addressbook.db")
 	if err != nil {
 		return err
@@ -35,12 +37,21 @@ func run() error {
 			return err
 		}
 
-		if err := inputRecord(db); err != nil {
-			return err
+		fmt.Print("Change Record? (Y/n)")
+		fmt.Scan(&input)
+
+		if input == "y" {
+			if err := editRecord(db); err != nil {
+				return err
+			}
 		}
 
-		if err := editRecord(db); err != nil {
-			return err
+		fmt.Print("Add Record? (Y/n)")
+		fmt.Scan(&input)
+		if input == "y" {
+			if err := inputRecord(db); err != nil {
+				return err
+			}
 		}
 	}
 
@@ -73,26 +84,9 @@ func showRecords(db *sql.DB) error {
 		if err := rows.Scan(&r.ID, &r.Name, &r.Phone); err != nil {
 			return err
 		}
-		fmt.Printf("[%d] Name:%s TEL:%s\n", r.ID, r.Name, r.Phone)
+		fmt.Printf("[%d]  Name:%s TEL:%s\n", r.ID, r.Name, r.Phone)
 	}
 	fmt.Println("--------")
-
-	return nil
-}
-
-func inputRecord(db *sql.DB) error {
-	var r Record
-	fmt.Print("Name >")
-	fmt.Scan(&r.Name)
-
-	fmt.Print("TEL >")
-	fmt.Scan(&r.Phone)
-
-	const sql = "INSERT INTO addressbook(name, phone) values (?,?)"
-	_, err := db.Exec(sql, r.Name, r.Phone)
-	if err != nil {
-		return err
-	}
 
 	return nil
 }
@@ -100,12 +94,63 @@ func inputRecord(db *sql.DB) error {
 func editRecord(db *sql.DB) error {
 	var input string
 	var r Record
-	fmt.Print("change Record? (Y/n)")
+	fmt.Print("ID >")
+	fmt.Scan(&r.ID)
+	// IDに紐ずくレコードを取得
+	rows := db.QueryRow("SELECT * FROM addressbook WHERE ID = ?", r.ID)
+	if err := rows.Scan(&r.ID, &r.Name, &r.Phone); err != nil {
+		return err
+	}
+	fmt.Printf("[%d]  Name:%s TEL:%s\n", r.ID, r.Name, r.Phone)
+
+	fmt.Print("Which Column Change? (name / TEL)")
 	fmt.Scan(&input)
 
-	if input == "y" {
-		fmt.Print("ID >")
-		fmt.Scan(&r.ID)
+	switch input {
+	case "name":
+		fmt.Print("Enter the changed name ? >")
+		fmt.Scan(&r.Name)
+		r, err := db.Exec("UPDATE addressbook SET name = ? WHERE ID = ? ", r.Name, r.ID)
+		if err != nil {
+			log.Println(err)
+		}
+		cnt, err := r.RowsAffected()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println("Affected rows:", cnt)
+	case "TEL":
+		fmt.Print("Enter the changed TEL ? >")
+		fmt.Scan(&r.Phone)
+		r, err := db.Exec("UPDATE addressbook SET TEL = ? WHERE ID = ? ", r.Phone, r.ID)
+		if err != nil {
+			log.Println(err)
+		}
+		cnt, err := r.RowsAffected()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println("Affected rows:", cnt)
+	default:
+		fmt.Print("Sorry, Invalid command")
+		return nil
 	}
+	return nil
+}
+
+func inputRecord(db *sql.DB) error {
+	var r Record
+
+	fmt.Print("Name >")
+	fmt.Scan(&r.Name)
+
+	fmt.Print("TEL >")
+	fmt.Scan(&r.Phone)
+	const sql = "INSERT INTO addressbook(name, phone) values (?,?)"
+	_, err := db.Exec(sql, r.Name, r.Phone)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
